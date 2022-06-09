32be678a806d1e4e9ea1a6d9fb97d328aca676a0
MSKose <saitmustafa1998@gmail.com>
2022-06-09 12:44:42 +0200

    added live and onlick functionality

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ const button = document.getElementById('btn');
 console.log(button);
 
 let number = Math.floor(Math.random() * 100 + 1);
+console.log(number);
+let live = 5;
 
 button.addEventListener('click', () => {
     if (!inputText.value) {
         return alert('Please enter your guess')
     }else {
-        return 1
+        if (inputText.value == number){
+            alert(`congrats, you're guess is right`);
+            inputText.value = ''
+        }else {
+            live--
+            inputText.value = ''
+            prompt.textContent = `You've got ${live} lives left, think wisely`
+            console.log(`live: ${live}`);
+        }
     }
 })
 
